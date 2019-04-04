77aa37a17cdd2b6fa476e72b0b34b0f3f5fc31fb
Ding Yi <dyihoon90@gmail.com>
2019-04-04 19:20:06 +0800

    initial commit

--- src/myinfo/index.ts
+++
@@ -1,9 +0,0 @@
-export {
-	MyInfoHelper as Helper, MyInfoHelperConstructor as HelperConstructor,
-	MyInfoRequest as Request, MyInfoRequestConstructor as MyInfoRequestConstructor,
-} from "./helper";
-
-import * as Fake from "./fake";
-export { Fake };
-
-export { AddressLocal, ChildBirthRecord, Person, PhoneNumLocal } from "./domain";
--- src/util/index.ts
+++
@@ -1,7 +0,0 @@
-import * as ConfigUtils from "./ConfigUtils";
-import * as StringUtil from "./StringUtil";
-import * as JestUtil from "./JestUtil";
-import * as SigningUtil from "./SigningUtil";
-import * as JweUtil from "./JweUtil";
-
-export { ConfigUtils, StringUtil, JestUtil, SigningUtil, JweUtil };
