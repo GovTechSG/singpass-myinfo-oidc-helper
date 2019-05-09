02bab698d41b329f9377bc90da987b86a58d52ae
Ding Yi <dyihoon90@gmail.com>
2019-05-09 16:55:46 +0800

    [MOL-1814][DY|JQ] add factories for myinfo person basic

--- src/myinfo/fake/index.ts
+++ src/myinfo/fake/index.ts
@@ -1,3 +1,5 @@
+import * as factory from "./factory/personbasic-factory";
+export { factory };
 export { FakeMyInfoHelper as Helper, IFakeMyInfoHelper as IHelper, MockParams } from "./fake-helper";
 export { FakeMyInfoRequest as Request } from "./fake-request";
 export * from "./profiles/fake-profiles";
