59b1ca332da60ee936d57d1c9aa8406338b8eb52
Jasmine <jasqxl94@gmail.com>
2019-05-17 12:02:43 +0800

    [MISC][JasQ] Fix spelling error

--- src/myinfo/domain/profilestatus-domain.ts
+++ src/myinfo/domain/profilestatus-domain.ts
@@ -1,5 +1,5 @@
 
-export enum ProfileStautsEnum {
+export enum ProfileStatusEnum {
 	NO_PROFILE = "01",
 	INCOMPLETE_VERIFIED_FIELDS = "02",
 	COMPLETE_VERIFIED_FIELDS = "03",
@@ -8,5 +8,5 @@ export enum ProfileStautsEnum {
 
 export interface ProfileStatus {
 	message: string;
-	status: ProfileStautsEnum;
+	status: ProfileStatusEnum;
 }
