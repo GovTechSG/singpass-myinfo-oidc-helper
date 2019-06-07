4e1921e85552ef093aa892787fb86e07dc1c2c0a
MOL Mac Pro <molmacpro@ip-192-168-109-195.ap-southeast-1.compute.internal>
2019-06-07 18:03:20 +0800

    Built package based on: Version: v2.2.1 - Bamboo build: 62

--- src/myinfo/fake/index.ts
+++
@@ -1,6 +0,0 @@
-import * as factory from "./factory/personbasic-factory";
-export { factory };
-export { FakeMyInfoHelper as Helper, IFakeMyInfoHelper as IHelper, MockParams } from "./fake-helper";
-export { FakeMyInfoRequest as Request } from "./fake-request";
-export * from "./profiles/fake-profiles";
-export { ProfileArchetype } from "./profiles/fake-profile";
