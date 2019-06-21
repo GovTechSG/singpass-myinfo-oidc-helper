30790ef17fad7297bc89eaa3db7ef04a778a721d
Ubuntu <ubuntu@ip-10-1-27-210.ap-southeast-1.compute.internal>
2019-06-21 17:12:18 +0800

    Built package based on: Version: v2.2.12 - Bamboo build: 81

--- webpack.watch.config.ts
+++
@@ -1,8 +0,0 @@
-import { createBaseLibWatchConfig } from "./shared-config/lib.webpack.watch.config";
-
-const webPackConfig = createBaseLibWatchConfig({
-	name: "singpass-myinfo-oidc-helper",
-	dir: "./",
-});
-
-export default webPackConfig;
