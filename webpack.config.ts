867a23c15936bec30b8631d66a04f2a12182f8eb
Ubuntu <ubuntu@ip-10-1-27-210.ap-southeast-1.compute.internal>
2019-06-25 11:06:43 +0800

    Built package based on: Version: v2.2.16 - Bamboo build: 85

--- webpack.config.ts
+++
@@ -1,8 +0,0 @@
-import { createBaseLibConfig } from "./shared-config/lib.webpack.config";
-
-const webPackConfig = createBaseLibConfig({
-	name: "singpass-myinfo-oidc-helper",
-	dir: "./",
-});
-
-export default webPackConfig;
