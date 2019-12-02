2dc210bce5d7e85d5ce7cb2b3a1edae387cc23d3
MOL Mac Pro <molmacpro@ip-10-10-1-153.ap-southeast-1.compute.internal>
2019-12-02 11:34:27 +0800

    Built package | Version: v3.0.0 | Bamboo: singpass-myinfo master master #208

---
+++ util/StringUtil.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const isBase64 = require("is-base64");
+function replaceLineBreaks(input) {
+    if (!input) {
+        return input;
+    }
+    return input.replace(/\\n/g, "\n");
+}
+exports.replaceLineBreaks = replaceLineBreaks;
+function isBase64Valid(test) {
+    return isBase64(test, { paddingRequired: false });
+}
+exports.isBase64Valid = isBase64Valid;
+//# sourceMappingURL=StringUtil.js.map
