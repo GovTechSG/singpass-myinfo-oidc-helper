6da2e29b102560dcc5e943c6695a470a3df18149
MOL Mac Pro <molmacpro@ip-192-168-1-76.ap-southeast-1.compute.internal>
2020-05-04 11:45:33 +0800

    Built package | Version: v3.1.7 | Bamboo: singpass-myinfo master master #279

---
+++ client/index.js
@@ -0,0 +1,54 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const axios_1 = require("axios");
+const ProxyAgent = require("proxy-agent");
+const RedactorUtil_1 = require("../util/RedactorUtil");
+exports.createClient = (requestConfig = {}) => {
+    // Note: Due to axios not being able to automatically pick up proxy env vars, we have to manually define a custom proxy agent
+    // Axios 0.19.0 has not fixed this issue yet, so we are using this workaround: https://github.com/axios/axios/issues/925#issuecomment-419352052
+    const proxyConfig = getProxyConfig();
+    if (!!proxyConfig) {
+        const proxyAgent = new ProxyAgent(proxyConfig);
+        requestConfig = Object.assign({ httpAgent: proxyAgent, httpsAgent: proxyAgent, proxy: false }, requestConfig);
+    }
+    const instance = axios_1.default.create(Object.assign({}, requestConfig));
+    addRequestLogs(instance);
+    addResponseLogs(instance);
+    return instance;
+};
+const getProxyConfig = () => {
+    return process.env.HTTPS_PROXY || process.env.https_proxy || process.env.HTTP_PROXY || process.env.http_proxy;
+};
+const addRequestLogs = (instance) => {
+    instance.interceptors.request.use((request) => {
+        if (!!request) {
+            log(`Requesting ${request.method} : ${RedactorUtil_1.redactNricfinInString(request.url)}`);
+        }
+        return request;
+    }, (error) => {
+        log(`Error occurred while making a request ${error.message}`, error);
+        return Promise.reject(error);
+    });
+};
+const addResponseLogs = (instance) => {
+    instance.interceptors.response.use((response) => {
+        if (!!response) {
+            log(`Responded ${response.config.method}  : ${RedactorUtil_1.redactNricfinInString(response.config.url)}`);
+        }
+        return response;
+    }, (error) => {
+        if (!!error.response) {
+            log(`Error occurred while responding to request ${error.response.config.method} :
+			${RedactorUtil_1.redactNricfinInString(error.response.config.url)}`, error);
+        }
+        else {
+            log(`Error occurred while responding to request`, error);
+        }
+        return Promise.reject(error);
+    });
+};
+const log = (message, ...optionalParams) => {
+    // tslint:disable-next-line:no-console
+    console.log(message, optionalParams);
+};
+//# sourceMappingURL=index.js.map
---
+++ myinfo/fake/profiles/mrGSTVUncleScenario1a.js
@@ -0,0 +1,23 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const _ = require("lodash");
+const fake_profile_1 = require("./fake-profile");
+const mrSGDaddyPerfect_1 = require("./mrSGDaddyPerfect");
+const id = "S1234567D";
+const name = fake_profile_1.ProfileArchetype.MR_SG_UNCLE_GST_SCENARIO_1A;
+exports.mrGstvUncle1a = {
+    id,
+    name,
+    // tslint:disable-next-line: no-big-function
+    generate: (profileName) => {
+        profileName = _.isEmpty(profileName) ? name : profileName;
+        const profile = mrSGDaddyPerfect_1.mrSGDaddyPerfect.generate(profileName);
+        profile.gstvoucher.exclusion.value = false;
+        profile.gstvoucher.signup.value = true;
+        profile.gstvoucher.gstregular.value = 100;
+        profile.gstvoucher.gstmedisave.value = 300;
+        profile.gstvoucher.gstspecial.value = 300;
+        return profile;
+    },
+};
+//# sourceMappingURL=mrGSTVUncleScenario1a.js.map
