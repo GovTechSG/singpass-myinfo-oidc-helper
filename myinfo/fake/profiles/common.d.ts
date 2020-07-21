5d75ac334e9947dabbc4c42a2d8c9e0731630086
MOL Mac Pro <molmacpro@ip-192-168-1-76.ap-southeast-1.compute.internal>
2020-07-21 11:26:11 +0800

    Built package | Version: v3.2.4 | Bamboo: singpass-myinfo master master #293

---
+++ myinfo/fake/profiles/common.d.ts
@@ -0,0 +1,9 @@
+export declare const unavailableField: UnavailableField;
+interface UnavailableField {
+    classification: "C";
+    source: "1" | "2" | "3" | "4";
+    lastupdated: string;
+    unavailable: boolean;
+}
+export {};
+//# sourceMappingURL=common.d.ts.map
