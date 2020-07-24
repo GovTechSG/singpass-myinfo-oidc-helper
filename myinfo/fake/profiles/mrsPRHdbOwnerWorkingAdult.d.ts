9163720a912ed15465f67e033d17a22d7adf117a
MOL Mac Pro <molmacpro@ip-192-168-1-76.ap-southeast-1.compute.internal>
2020-07-24 12:29:31 +0800

    Built package | Version: v3.2.8 | Bamboo: singpass-myinfo master master #301

---
+++ myinfo/fake/profiles/mrsPRHdbOwnerWorkingAdult.d.ts
@@ -0,0 +1,18 @@
+import { FakeProfile } from "./fake-profile";
+/**
+ * ArcheType:
+ * Age Range 1979-05-16
+ * Residential Status	P
+ * Sex	F
+ * Marital Status	2
+ * Local Registered Birth Records	3
+ * HDB Ownership	Yes
+ * Ownership of Private Residential Property	No
+ * Type of Housing for Registered Address	null
+ * Driving License	Yes
+ * Vehicle Ownership	Yes
+ * Highest Education Level	7
+ * Monthly Household Income	5500
+ */
+export declare const mrsPRHdbOwnerWorkingAdult: FakeProfile;
+//# sourceMappingURL=mrsPRHdbOwnerWorkingAdult.d.ts.map
