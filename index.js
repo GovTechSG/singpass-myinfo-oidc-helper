bb8b26c644d5cd6785a65766f43352bc23058677
MOL Mac Pro <molmacpro@ip-192-168-1-76.ap-southeast-1.compute.internal>
2020-09-17 11:03:06 +0800

    Built package | Version: v4.0.0 | Bamboo: singpass-myinfo master master #322

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const MyInfo = require("./myinfo");
+exports.MyInfo = MyInfo;
+const Singpass = require("./singpass");
+exports.Singpass = Singpass;
+//# sourceMappingURL=index.js.map
