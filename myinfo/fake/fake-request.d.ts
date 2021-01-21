9f55317f26fb18e09e305b8517687ec8eda9b5e1
Linux User <alpine@ip-172-16-237-239.ap-southeast-1.compute.internal>
2021-01-21 19:00:06 +0800

    Built package | Version: v4.2.19 | Bamboo: singpass-myinfo master master #378

---
+++ myinfo/fake/fake-request.d.ts
@@ -0,0 +1,8 @@
+import * as request from "request";
+import { IMyInfoRequest } from "../helper";
+export declare class FakeMyInfoRequest implements IMyInfoRequest {
+    constructor();
+    get(expectedResponse: any): Promise<request.RequestResponse>;
+    postForm(expectedResponse: any): Promise<request.RequestResponse>;
+}
+//# sourceMappingURL=fake-request.d.ts.map
