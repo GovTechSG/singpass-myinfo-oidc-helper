947bf2d5433dc85e6fc8c536d53a70c359159d55
Linux User <alpine@ip-172-16-237-137.ap-southeast-1.compute.internal>
2021-01-22 16:39:38 +0800

    Built package | Version: v4.2.20 | Bamboo: singpass-myinfo master master #379

---
+++ myinfo/fake/profiles/mrSGUncleMgNeedMedisave.js
@@ -0,0 +1,27 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.mrSGUncleMgNeedMedisave = void 0;
+const _ = require("lodash");
+const fake_profile_1 = require("./fake-profile");
+const mrSGDaddyPerfect_1 = require("./mrSGDaddyPerfect");
+const id = "S8399420G";
+const name = fake_profile_1.ProfileArchetype.MR_SG_UNCLE_MG_NEED_MEDISAVE;
+exports.mrSGUncleMgNeedMedisave = {
+    id,
+    name,
+    // tslint:disable-next-line: no-big-function
+    generate: (profileName) => {
+        profileName = _.isEmpty(profileName) ? name : profileName;
+        const profile = mrSGDaddyPerfect_1.mrSGDaddyPerfect.generate(profileName);
+        profile.merdekagen.message.code = "4";
+        profile.merdekagen.eligibility.value = true;
+        profile.dob = {
+            lastupdated: "2020-01-01",
+            source: "1",
+            classification: "C",
+            value: `${new Date().getFullYear() - 61}-01-01`,
+        };
+        return profile;
+    },
+};
+//# sourceMappingURL=mrSGUncleMgNeedMedisave.js.map
