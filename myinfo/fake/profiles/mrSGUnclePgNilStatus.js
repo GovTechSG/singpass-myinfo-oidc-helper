8781e7fb57beee211d369c4b2aa06d277fada479
Linux User <alpine@ip-10-11-3-164.ap-southeast-1.compute.internal>
2021-01-25 13:19:39 +0800

    Built package | Version: v4.2.21 | Bamboo: singpass-myinfo master master #380

---
+++ myinfo/fake/profiles/mrSGUnclePgNilStatus.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.mrSGUnclePgNilStatus = void 0;
+const _ = require("lodash");
+const fake_profile_1 = require("./fake-profile");
+const mrSGUnclePgAllOk_1 = require("./mrSGUnclePgAllOk");
+const id = "S8399420G";
+const name = fake_profile_1.ProfileArchetype.MR_SG_UNCLE_PG_NIL_STATUS;
+exports.mrSGUnclePgNilStatus = {
+    id,
+    name,
+    // tslint:disable-next-line: no-big-function
+    generate: (profileName) => {
+        profileName = _.isEmpty(profileName) ? name : profileName;
+        const profile = mrSGUnclePgAllOk_1.mrSGUnclePgAllOk.generate(profileName);
+        profile.pioneergen.message.code = "7";
+        profile.pioneergen.message.desc = "Please call 1800-2222-888 to start receiving your PG benefits.";
+        return profile;
+    },
+};
+//# sourceMappingURL=mrSGUnclePgNilStatus.js.map
