a5ea0e6e59e80e6bf30b50794498728ef995c844
Linux User <alpine@ip-172-16-237-162.ap-southeast-1.compute.internal>
2021-01-27 12:35:06 +0800

    Built package | Version: v4.2.22 | Bamboo: singpass-myinfo master master #382

---
+++ myinfo/fake/profiles/mrSGUncleMgCallReceive.js
@@ -0,0 +1,27 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.mrSGUncleMgCallReceive = void 0;
+const _ = require("lodash");
+const fake_profile_1 = require("./fake-profile");
+const mrSGDaddyPerfect_1 = require("./mrSGDaddyPerfect");
+const id = "S8399420G";
+const name = fake_profile_1.ProfileArchetype.MR_SG_UNCLE_MG_CALL_RECEIVE;
+exports.mrSGUncleMgCallReceive = {
+    id,
+    name,
+    // tslint:disable-next-line: no-big-function
+    generate: (profileName) => {
+        profileName = _.isEmpty(profileName) ? name : profileName;
+        const profile = mrSGDaddyPerfect_1.mrSGDaddyPerfect.generate(profileName);
+        profile.merdekagen.message.code = "5";
+        profile.merdekagen.eligibility.value = true;
+        profile.dob = {
+            lastupdated: "2020-01-01",
+            source: "1",
+            classification: "C",
+            value: `1959-01-01`,
+        };
+        return profile;
+    },
+};
+//# sourceMappingURL=mrSGUncleMgCallReceive.js.map
