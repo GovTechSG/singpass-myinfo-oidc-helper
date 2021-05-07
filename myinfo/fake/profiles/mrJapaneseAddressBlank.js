22f1dcdda45b4b59ea746ee3dd9635f20841072f
Linux User <alpine@ip-172-16-237-185.ap-southeast-1.compute.internal>
2021-05-07 11:19:39 +0800

    Built package | Version: v5.0.5 | Bamboo: singpass-myinfo master master #403

---
+++ myinfo/fake/profiles/mrJapaneseAddressBlank.js
@@ -0,0 +1,97 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.mrJapaneseAddressBlank = void 0;
+const _ = require("lodash");
+const domain_1 = require("../../domain");
+const fake_profile_1 = require("./fake-profile");
+const mrSGDaddyPerfect_1 = require("./mrSGDaddyPerfect");
+const id = "F5316757T";
+const name = fake_profile_1.ProfileArchetype.MR_JAPANESE_ADDRESS_BLANK;
+exports.mrJapaneseAddressBlank = {
+    id,
+    name,
+    generate: (profileName) => {
+        profileName = _.isEmpty(profileName) ? name : profileName;
+        const profile = mrSGDaddyPerfect_1.mrSGDaddyPerfect.generate(profileName);
+        profile.residentialstatus = {
+            "code": "",
+            "desc": "",
+            "source": "3",
+            "classification": "C",
+            "lastupdated": "",
+            "unavailable": false,
+        };
+        profile.nationality = {
+            "code": domain_1.MyInfoNationalityCode.JAPANESE,
+            "desc": domain_1.MyInfoNationalityCode.fn.toEnumDesc(domain_1.MyInfoNationalityCode.JAPANESE),
+            "source": "1",
+            "classification": "C",
+            "lastupdated": "2019-09-18",
+            "unavailable": false,
+        };
+        profile.birthcountry = {
+            "code": domain_1.MyInfoCountryCode.JAPAN,
+            "desc": domain_1.MyInfoCountryCode.fn.toEnumDesc(domain_1.MyInfoCountryCode.JAPAN),
+            "source": "1",
+            "classification": "C",
+            "lastupdated": "2019-09-18",
+            "unavailable": false,
+        };
+        profile.regadd = {
+            "type": "",
+            "unit": { "value": "" },
+            "block": { "value": "" },
+            "floor": { "value": "" },
+            "postal": { "value": "" },
+            "street": { "value": "" },
+            "building": { "value": "" },
+            "country": {
+                "code": "",
+                "desc": "",
+            },
+            "classification": "C",
+            "source": "3",
+            "lastupdated": "2018-05-10",
+        };
+        profile.dob = {
+            "lastupdated": "2018-06-01",
+            "source": "1",
+            "classification": "C",
+            "value": "1983-10-06",
+            "unavailable": false,
+        };
+        profile.marital = {
+            "lastupdated": "2020-09-10",
+            "code": null,
+            "source": "2",
+            "classification": "C",
+            "desc": null,
+            "unavailable": false,
+        };
+        profile.marriagedate = {
+            "lastupdated": "2020-09-10",
+            "source": "2",
+            "classification": "C",
+            "value": "",
+            "unavailable": false,
+        };
+        profile.occupation = {
+            "lastupdated": "2018-05-21",
+            "code": domain_1.MyInfoOccupationCode.LEGISLATOR,
+            "source": "2",
+            "classification": "C",
+            "desc": domain_1.MyInfoOccupationCode.fn.toEnumDesc(domain_1.MyInfoOccupationCode.LEGISLATOR),
+            "unavailable": false,
+        };
+        profile.dialect = {
+            "lastupdated": "2020-09-10",
+            "code": "",
+            "source": "2",
+            "classification": "C",
+            "desc": "",
+            "unavailable": false,
+        };
+        return profile;
+    },
+};
+//# sourceMappingURL=mrJapaneseAddressBlank.js.map
