13837153779b33742989fed3b14f8919684250e7
Linux User <alpine@ip-172-16-237-162.ap-southeast-1.compute.internal>
2021-05-28 21:12:53 +0800

    Built package | Version: v5.1.6 | Bamboo: singpass-myinfo master master #413

--- src/util/index.ts
+++
@@ -1,8 +0,0 @@
-import * as ConfigUtils from "./ConfigUtils";
-import * as JestUtil from "./JestUtil";
-import * as JweUtil from "./JweUtil";
-import { Logger } from "./Logger";
-import * as SigningUtil from "./SigningUtil";
-import * as StringUtil from "./StringUtil";
-
-export { ConfigUtils, StringUtil, JestUtil, SigningUtil, JweUtil, Logger };
