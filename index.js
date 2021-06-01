dbe4b676b521caafa5074ce1e76a0aab4aea7661
Linux User <alpine@ip-172-16-237-239.ap-southeast-1.compute.internal>
2021-06-01 11:17:33 +0800

    Built package | Version: v5.1.7 | Bamboo: singpass-myinfo master master #414

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Singpass = exports.MyInfo = void 0;
+const MyInfo = require("./myinfo");
+exports.MyInfo = MyInfo;
+const Singpass = require("./singpass");
+exports.Singpass = Singpass;
+//# sourceMappingURL=index.js.map
