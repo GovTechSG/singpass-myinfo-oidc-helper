605f471f0be6a05e60a8297bb8548fa87122700a
Linux User <alpine@ip-172-16-237-235.ap-southeast-1.compute.internal>
2021-10-26 11:36:02 +0800

    Built package | Version: v5.2.4 | Bamboo: singpass-myinfo master master #440

---
+++ myinfo/helper/myinfo-request.d.ts
@@ -0,0 +1,25 @@
+import { AxiosResponse } from "axios";
+export interface MyInfoRequestConstructor {
+    appId: string;
+    privateKeyToSignRequest: string;
+    privateKeyPassword: string;
+}
+/**
+ * For most cases, myinfo-helper will suffice.
+ * This class is used only for special MyInfo endpoints.
+ * Request class for MyInfo endpoints other than those defined in MyInfoHelper
+ */
+export declare class MyInfoRequest {
+    private readonly axiosClient;
+    private readonly appId;
+    private readonly privateKeyContent;
+    private readonly privateKeyPassword;
+    constructor(props: MyInfoRequestConstructor);
+    get<T>(uri: string, queryParams?: {
+        [key: string]: any;
+    }, accessToken?: string): Promise<AxiosResponse<T>>;
+    post<T>(uri: string, params: {
+        [key: string]: any;
+    }): Promise<AxiosResponse<T>>;
+}
+//# sourceMappingURL=myinfo-request.d.ts.map
