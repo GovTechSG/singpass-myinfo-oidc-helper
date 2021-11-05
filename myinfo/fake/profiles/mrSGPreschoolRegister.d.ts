23f7b9a0402a009ea9c656ed053c0eacf5f4cf82
Linux User <alpine@ip-172-16-237-136.ap-southeast-1.compute.internal>
2021-11-05 16:08:58 +0800

    Built package | Version: v5.2.5 | Bamboo: singpass-myinfo master master #441

---
+++ myinfo/fake/profiles/mrSGPreschoolRegister.d.ts
@@ -0,0 +1,12 @@
+import { MyInfoComponents } from "../../domain";
+import { FakeProfile } from "./fake-profile";
+export declare const mrPreschoolRegister: FakeProfile;
+export declare const ChildrenRecords: {
+    c1: MyInfoComponents.Schemas.Childrenbirthrecords;
+    c2: MyInfoComponents.Schemas.Childrenbirthrecords;
+    c3: MyInfoComponents.Schemas.Childrenbirthrecords;
+    c4: MyInfoComponents.Schemas.Childrenbirthrecords;
+    c5: MyInfoComponents.Schemas.Childrenbirthrecords;
+    c6: MyInfoComponents.Schemas.Childrenbirthrecords;
+};
+//# sourceMappingURL=mrSGPreschoolRegister.d.ts.map
