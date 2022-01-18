60da41370e251003cd221cc38a07867c87c9456f
Linux User <alpine@ip-172-16-237-94.ap-southeast-1.compute.internal>
2022-01-18 18:52:28 +0800

    Built package | Version: v6.0.0 | Bamboo: singpass-myinfo master master #447

---
+++ util/Logger.d.ts
@@ -0,0 +1,9 @@
+export declare const logger: {
+    log: (message: string, param?: any) => void;
+    debug: (message: string, param?: any) => void;
+    info: (message: string, param?: any) => void;
+    trace: (message: string, param?: any) => void;
+    warn: (message: string, param?: any) => void;
+    error: (message: string, param?: any) => void;
+};
+//# sourceMappingURL=Logger.d.ts.map
