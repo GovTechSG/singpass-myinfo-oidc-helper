2ef8b79955a0a1e4ad558971f09d26f1f418cbd0
Linux User <alpine@ip-172-16-237-179.ap-southeast-1.compute.internal>
2022-02-22 10:39:38 +0800

    Built package | Version: v6.1.2 | Bamboo: singpass-myinfo master master #450

---
+++ dist/myinfo/domain/generated/myinfo-pass-type-code.d.ts
@@ -0,0 +1,27 @@
+export declare enum MyInfoPassTypeCode {
+    "WORK_PERMIT" = "RPass",
+    "S_PASS" = "SPass",
+    "EMPLOYMENT_PASS" = "P1Pass",
+    "EMPLOYMENT_PASS_2" = "P2Pass",
+    "EMPLOYMENT_PASS_3" = "QPass",
+    "PERSONALISED_EMPLOYMENT_PASS" = "PEP",
+    "WORK_HOLIDAY_PASS" = "WHP",
+    "TRAINING_EMPLOYMENT_PASS" = "TEP",
+    "ENTRE_PASS" = "Entre",
+    "DEPENDENT_PASS" = "DP",
+    "LONG_TERM_VISIT_PASS" = "LTVP",
+    "LETTER_OF_CONSENT" = "LOC",
+    "MISCELLANEOUS_WORK_PASS" = "MWP",
+    "SHORT_TERM_PASS" = "STP",
+    "LONG_TERM_VISIT_PASS_2" = "LTVP+"
+}
+export declare namespace MyInfoPassTypeCode {
+    const fn: {
+        keys: () => string[];
+        values: () => MyInfoPassTypeCode[];
+        toEnumKey: (value: MyInfoPassTypeCode) => string;
+        toEnumValue: (value: string | number) => MyInfoPassTypeCode;
+        toEnumDesc: (value: MyInfoPassTypeCode) => "WORK PERMIT" | "S PASS" | "EMPLOYMENT PASS" | "PERSONALISED EMPLOYMENT PASS" | "WORK HOLIDAY PASS" | "TRAINING EMPLOYMENT PASS" | "ENTREPASS" | "DEPENDENT PASS" | "LONG TERM VISIT PASS" | "LETTER OF CONSENT" | "MISCELLANEOUS WORK PASS" | "SHORT TERM PASS" | "LONG TERM VISIT PASS +";
+    };
+}
+//# sourceMappingURL=myinfo-pass-type-code.d.ts.map
---
+++ dist/util/index.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.JweUtil = exports.SigningUtil = exports.JestUtil = exports.StringUtil = exports.ConfigUtils = void 0;
+const ConfigUtils = require("./ConfigUtils");
+exports.ConfigUtils = ConfigUtils;
+const JestUtil = require("./JestUtil");
+exports.JestUtil = JestUtil;
+const JweUtil = require("./JweUtil");
+exports.JweUtil = JweUtil;
+const SigningUtil = require("./SigningUtil");
+exports.SigningUtil = SigningUtil;
+const StringUtil = require("./StringUtil");
+exports.StringUtil = StringUtil;
+//# sourceMappingURL=index.js.map
