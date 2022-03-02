b3caf4acb3a6db3fa3a0b3f5e508ddd27799f0ff
Linux User <alpine@ip-172-16-237-153.ap-southeast-1.compute.internal>
2022-03-02 16:53:33 +0800

    Built package | Version: v6.1.9 | Bamboo: singpass-myinfo master master #457

---
+++ dist/index.d.ts
@@ -0,0 +1,5 @@
+import * as MyInfo from "./myinfo";
+import * as Singpass from "./singpass";
+import * as Util from "./util";
+export { MyInfo, Singpass, Util };
+//# sourceMappingURL=index.d.ts.map
