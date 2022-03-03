38e4a3a2b47608ca2a079a9e7abe613ad76953f8
Linux User <alpine@ip-172-16-237-107.ap-southeast-1.compute.internal>
2022-03-03 15:23:49 +0800

    Built package | Version: v6.1.10 | Bamboo: singpass-myinfo master master #458

---
+++ dist/myinfo/fake/profiles/mrICADaddyScenario7.d.ts
@@ -0,0 +1,3 @@
+import { FakeProfile } from "./fake-profile";
+export declare const mrICADaddyScenario7: FakeProfile;
+//# sourceMappingURL=mrICADaddyScenario7.d.ts.map
--- jest.ext.config.js
+++
@@ -1,7 +0,0 @@
-const baseConfig = require("./shared-config/jest.config");
-
-const config = {
-	...baseConfig.createBaseConfig(baseConfig.TestType.EXTERNAL),
-};
-
-module.exports = config;
