7f51d810447087dfee1e60243162b0eaf5d40ebf
Linux User <alpine@ip-172-16-237-139.ap-southeast-1.compute.internal>
2022-08-01 14:36:32 +0800

    Built package | Version: v6.2.2 | Bamboo: singpass-myinfo master master #464

---
+++ dist/myinfo/fake/profiles/mrICADaddyScenario3.js
@@ -0,0 +1,75 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.mrICADaddyScenario3 = void 0;
+const _ = require("lodash");
+const fake_profile_1 = require("./fake-profile");
+const domain_1 = require("../../domain");
+const BaseProfile_1 = require("./sponsored-children/BaseProfile");
+const id = "S9005006J";
+const name = fake_profile_1.ProfileArchetype.MR_ICA_SG_DADDY_MAIN_SCENARIO_3;
+exports.mrICADaddyScenario3 = {
+    id,
+    name,
+    generate: (profileName) => {
+        profileName = _.isEmpty(profileName) ? name : profileName;
+        const profile = BaseProfile_1.BaseProfile.generate(profileName);
+        profile.sex.code = domain_1.MyInfoSexCode.MALE;
+        profile.sex.desc = domain_1.MyInfoSexCode.fn.toEnumDesc(domain_1.MyInfoSexCode.MALE);
+        profile.race = {
+            "lastupdated": "2022-02-25",
+            "code": domain_1.MyInfoRaceCode.INDIAN,
+            "desc": domain_1.MyInfoRaceCode.fn.toEnumDesc(domain_1.MyInfoRaceCode.INDIAN),
+            "source": "1",
+            "classification": "C",
+            "unavailable": false,
+        },
+            profile.dob = {
+                "lastupdated": "2022-02-25",
+                "source": "1",
+                "classification": "C",
+                "value": "1990-02-03",
+                "unavailable": false,
+            };
+        profile.residentialstatus = {
+            "lastupdated": "2018-06-01",
+            "code": domain_1.MyInfoResidentialCode.CITIZEN,
+            "desc": domain_1.MyInfoResidentialCode.fn.toEnumDesc(domain_1.MyInfoResidentialCode.CITIZEN),
+            "source": "1",
+            "classification": "C",
+            "unavailable": false,
+        },
+            profile.nationality.code = domain_1.MyInfoNationalityCode.SINGAPORE_CITIZEN;
+        profile.marriagecertno = {
+            "lastupdated": "2022-02-25",
+            "source": "1",
+            "classification": "C",
+            "value": "M34567",
+            "unavailable": false,
+        };
+        profile.marriagedate = {
+            "lastupdated": "2020-09-10",
+            "source": "2",
+            "classification": "C",
+            "value": "2020-09-10",
+            "unavailable": false,
+        };
+        profile.countryofmarriage = {
+            "lastupdated": "2022-02-25",
+            "code": domain_1.MyInfoCountryCode.SINGAPORE,
+            "desc": domain_1.MyInfoCountryCode.fn.toEnumDesc(domain_1.MyInfoCountryCode.SINGAPORE),
+            "source": "1",
+            "classification": "C",
+            "unavailable": false,
+        };
+        profile.marital = {
+            "lastupdated": "2022-02-25",
+            "code": domain_1.MyInfoMaritialStatusCode.MARRIED,
+            "desc": domain_1.MyInfoMaritialStatusCode.fn.toEnumDesc(domain_1.MyInfoMaritialStatusCode.MARRIED),
+            "source": "1",
+            "classification": "C",
+            "unavailable": false,
+        };
+        return profile;
+    }
+};
+//# sourceMappingURL=mrICADaddyScenario3.js.map
