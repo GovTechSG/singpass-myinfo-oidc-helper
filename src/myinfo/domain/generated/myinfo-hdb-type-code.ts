987e342f1080da26373160bd239a4116191d0fc1
Hamiriza <holilhamiriza@gmail.com>
2023-05-10 17:03:46 +0800

    [MOL-12805][HFAH] run generate typings based on myinfo

--- src/myinfo/domain/generated/myinfo-hdb-type-code.ts
+++ src/myinfo/domain/generated/myinfo-hdb-type-code.ts
@@ -1,6 +1,6 @@
 // tslint:disable
 // =============================================================================
-// This file was generated with `npm run generate-myinfo-typings` on 2022-09-16
+// This file was generated with `npm run generate-myinfo-typings` on 2023-05-10
 // Any modifications to this file may be overwritten when the script runs again
 // Check README.md for more information
 // =============================================================================
