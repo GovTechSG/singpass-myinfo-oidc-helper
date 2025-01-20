611b37099213a8b74497aaaec5b0d5ef47246cf5
group_7001_bot_1 <group_7001_bot_1@noreply.sgts.gitlab-dedicated.com>
2025-01-20 13:31:18 +0800

    Built package | Version: v8.4.5 | Gitlab: singpass-myinfo-oidc-helper master 8191995

---
+++ dist/myinfo/fake/profiles/mrICADaddyScenario1.d.ts
@@ -0,0 +1,3 @@
+import { FakeProfile } from "./fake-profile";
+export declare const mrICADaddyScenario1: FakeProfile;
+//# sourceMappingURL=mrICADaddyScenario1.d.ts.map
---
+++ dist/myinfo/fake/profiles/mrMpResident3.d.ts
@@ -0,0 +1,3 @@
+import { FakeProfile } from "./fake-profile";
+export declare const mrMpResident3: FakeProfile;
+//# sourceMappingURL=mrMpResident3.d.ts.map
