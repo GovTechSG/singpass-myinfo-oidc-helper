e892dddbced1dc20f4da4b3ed4a45606b8ea59cb
Shawn Wee <shawn.wee90@hotmail.com>
2025-06-19 13:56:19 +0800

    [MISC][SW] export types from index

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import * as Corppass from "./corppass";
 import * as MyInfo from "./myinfo";
 import * as Singpass from "./singpass";
+import * as MyInfoTypes from "./types";
 import * as Util from "./util";
 
-export { Corppass, MyInfo, Singpass, Util };
+export { Corppass, MyInfo, MyInfoTypes, Singpass, Util };
